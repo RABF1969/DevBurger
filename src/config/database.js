55b6057e50be8d0b77fa0a3f6798f28391526131
Antônio Ferreira <rabf44@gmail.com>
2025-05-22 10:31:18 -0300

    configuração do sequelize

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres', 
+  password: '2024',
+  database: 'postgres',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+}
